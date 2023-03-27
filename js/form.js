23b35bc2f23dc5fab8fa94b0b5b6d99da1794610
Cerasus04 <cerasus1004@gmail.com>
2023-03-27 15:28:38 +0200

    На проверку после ревью

--- js/form.js
+++ js/form.js
@@ -56,48 +56,15 @@ const hashtagsHandler = (value) => {
 
   errorMessage = '';
 
-  const inputText = value.toLowerCase().trim();
-
-  if(!inputText) {
-    return true;
-  }
-
-  const inputArray = inputText.split(/\s+/);
+  const inputArray = value.toLowerCase().trim().split(/\s+/);
 
   if (inputArray.length === 0) {
     return true;
   }
 
-  // const rules = [
-  //   {
-  //     check: inputArray.some((item) => item.indexOf('#', 1) >= 1),
-  //     error: 'Хэш-теги должны разделяться пробелами',
-  //   },
-  //   {
-  //     check: inputArray.some((item) => item[0] !== '#'),
-  //     error: 'Хэш-тег должен начинаться с символа #',
-  //   },
-  //   {
-  //     check: inputArray.some((item, num, arr) => arr.includes(item, num + 1)),
-  //     error: 'Хэш-теги не должны повторяться',//проверка на уникальность тэгов
-  //   },
-  //   {
-  //     check: inputArray.some((item) => item.length > MAX_SYMBOLS),//проверка на максимальное колличество символов
-  //     error: `Максимальная длина одного хэш-тэга ${MAX_SYMBOLS} символов, включая решётку;`,
-  //   },
-  //   {
-  //     check: inputArray.length > MAX_HASHTAGS,
-  //     error: `Нельзя указывать больше ${MAX_HASHTAGS} хэш-тэгов`,
-  //   },
-  //   {
-  //     check: inputArray.some((item) => !/^#[a-zа-яё0-9]{1,19}$/i.test(item)),//проверка на допустимые символы
-  //     error: 'Хэш-тэг содержит недопустимые символы',
-  //   },
-  // ];
-
   return rules.every((rule) => {
 
-    const isInvalid = rule.check;
+    const isInvalid = rule.check(inputArray);
 
     if (isInvalid) {
       errorMessage = rule.error;
